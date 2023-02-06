3532e020986ce0d57ead0e509a7465255a83bbd8
Adam Taylor <v-adamtaylor@microsoft.com>
2023-02-06 15:56:41 -0600

    Another commit

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,8 +6,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    myAppSetting = os.environ["myAppSetting"]
-    logging.info(f'My app setting value:{myAppSetting}')
+    myAppSetting = os.environ.get('myAppSetting')
 
     if myAppSetting:
         return func.HttpResponse(f"myAppSetting is set to {myAppSetting}", status_code=200)
