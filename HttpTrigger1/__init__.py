b3221b10ef6724058d55599dfc4c9b6cb091be56
Adam Taylor <v-adamtaylor@microsoft.com>
2023-02-06 15:15:26 -0600

    Added App Settings Code to HttpTrigger1

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,24 +1,14 @@
 import logging
-
+import os
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    myAppSettingValue = os.environ.get('myAppSetting')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if myAppSettingValue:
+        return func.HttpResponse(f"myAppSetting is set to {myAppSettingValue}", status_code=200)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse("myAppSetting is not set", status_code=500)
